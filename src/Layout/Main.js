23381c1a938ffd03e875c054e2e8abb1e5d1337e
JubaStriker <jubastriker@gmail.com>
2022-11-25 18:03:31 +0600

    theme context added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import { ThemeContext } from '../Context/Theme/ThemeProvider';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const Main = () => {
+    const { toggleTheme } = useContext(ThemeContext)
+
+    let theme = "light"
+
+    if (toggleTheme) {
+        theme = "light";
+    }
+    else {
+        theme = "dark";
+    }
     return (
-        <div>
+        <div data-theme={`${theme}`}>
             <Navbar />
             <Outlet />
             <Footer />
